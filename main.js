32cf00ca6a19e99712826fceda14a53e2eb4e23d
Seppi <schuchmannsebastian@gmail.com>
2020-11-27 12:17:09 +0100

    Nice CSS and so on

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 var form = document.forms.namedItem("fileinfo");
+var lastImage;
 
 form.addEventListener('submit', function(ev) {
   ev.preventDefault()
     let formdata = new FormData(form);
-
+    
     var requestOptions = {
       method: 'POST',
       body: formdata,
     };
     
     fetch("https://1a3c5e234d38.ngrok.io/predict", requestOptions)
-       .then(response => console.log(response))
-       .then(result => console.log(result))
-       .catch(error => console.log(error));
+       .then(response => response.text().then((result =>{
+          console.log(result);
+          DisplayResult(result)
+       })));
 });
+
+//On Image Uploaded
+form.addEventListener('change', function(ev){
+  lastImage = URL.createObjectURL(ev.target.files[0]);
+  document.getElementById("uploadedImg").src = lastImage
+})
+
+function DisplayResult(result){
+  /*I have created some CSS classes to make this look half decent.*/
+  resultDiv = document.getElementById("results")
+
+  divContainer = document.createElement("div")
+  divContainer.className = "result"
+
+  divImage = document.createElement("div")
+  divImage.className = "image"
+  divImage.style.backgroundImage = "url('" + lastImage + "')";
+
+  divText = document.createElement("div")
+  divText.appendChild(document.createTextNode(result))
+  divText.className = "text"
+
+  divContainer.appendChild(divImage)
+  divContainer.appendChild(divText)
+  resultDiv.prepend(divContainer)
+}
+
 
--- main.py
+++ main.py
@@ -2,25 +2,19 @@ from flask import Flask, request
 from flask_cors import CORS
 from fastai.vision.all import *
 
-
 #Labeling function required for load_learner to work
 def GetLabel(fileName):
   return fileName.split('_')[0]
 
 learn = load_learner(Path('./export.pkl')) #Import Model
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app) #Request will get blocked otherwise on Localhost
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     img = PILImage.create(request.files['file'])
     label,_,probs = learn.predict(img)
-    print(label)
-    return label
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+    return f'{label} ({torch.max(probs).item()*100:.0f}%)'
 
 if __name__=='__main__':
     app.run()
